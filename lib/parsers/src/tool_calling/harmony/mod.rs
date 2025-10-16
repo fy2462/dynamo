15b5807b4b5506f7f6826d20950bb03d9a6fb232
zhongdaor <zhongdaor@nvidia.com>
2025-10-16 15:10:12 -0700

    fix: Migrate to new implementation that using parse_tool_calls_harmony_complete
    Signed-off-by: zhongdaor <zhongdaor@nvidia.com>

--- lib/parsers/src/tool_calling/harmony/mod.rs
+++ lib/parsers/src/tool_calling/harmony/mod.rs
@@ -6,7 +6,7 @@ pub mod harmony_parser;
 pub use super::config::JsonParserConfig;
 pub use super::{config, response};
 pub use harmony_parser::{
-    detect_tool_call_start_harmony, parse_tool_calls_harmony, parse_tool_calls_harmony_complete,
+    detect_tool_call_start_harmony, parse_tool_calls_harmony_complete,
 };
 
 pub fn find_tool_call_end_position_harmony(chunk: &str, config: &JsonParserConfig) -> usize {
