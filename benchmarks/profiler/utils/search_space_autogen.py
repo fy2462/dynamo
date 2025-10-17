687f6c7bdbb0d1f2cca4cd83a337cd9ae22b0de8
hongkuanz <hongkuanz@nvidia.com>
2025-10-17 09:25:00 -0700

    stage
    Signed-off-by: hongkuanz <hongkuanz@nvidia.com>

---
+++ benchmarks/profiler/utils/search_space_autogen.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import logging
+from deploy.utils.gpu_inventory import get_gpu_summary
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S"
+)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+def auto_generate_search_space(args: argparse.Namespace) -> None:
